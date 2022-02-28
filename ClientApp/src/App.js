6ddcbb2e8bd02a4ace53ae8ab3583c67d70d1c73
andreykarpov0 <akarpov863@gmail.com>
2022-02-28 17:36:25 +0300

    initial commit, asp net core 5.0 project

---
+++ ClientApp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <div>
+        привет
+      </div>
+    );
+  }
+}
