3aba3fd47b646f20928d4df0710ab857411b6b2f
lakilev <lev.cat.lev@mail.ru>
2022-03-02 18:51:47 +0300

    registrOne

--- ClientApp/src/windows/Reg.jsx
+++ ClientApp/src/windows/Reg.jsx
@@ -4,6 +4,22 @@ import './Reg.css';
 
 export default function Reg() {
   return (
-    <div>Reg</div>
+    <div>
+      <h1>регестрация</h1>
+      <form>
+        <p>Имя</p>
+        <input type="text"/>
+        <p>Фамилия</p>
+        <input type="text"/>
+        <p>Почта</p>
+        <input type="text"/>
+        <p>Логин</p>
+        <input type="text"/>
+        <p>Пароль</p>
+        <input type="password"/>
+        <br/>
+        <input type="submit"/>
+      </form>
+    </div>
   )
 }
