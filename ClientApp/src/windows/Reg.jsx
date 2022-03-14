fbc481d53613b869faf0c16d5d6394b3968a3e6c
lakilev <lev.cat.lev@mail.ru>
2022-03-14 18:29:22 +0300

    decompose for registration is done

--- ClientApp/src/windows/Reg.jsx
+++ ClientApp/src/windows/Reg.jsx
@@ -5,21 +5,20 @@ import './Reg.css';
 export default function Reg() {
   return (
     <div>
-      <h1>регистрация</h1>
-      <form>
-        <p>Имя</p>
-        <input type="text"/>
-        <p>Фамилия</p>
-        <input type="text"/>
-        <p>Почта</p>
-        <input type="text"/>
-        <p>Логин</p>
-        <input type="text"/>
-        <p>Пароль</p>
-        <input type="password"/>
-        <br/>
-        <input type="submit"/>
-      </form>
+      <p id="title">Chemex</p>
+        <div className="container"/>
+            <div className="reg_block">
+                <p id="reg_title">Регистрация</p>
+                <input id="log_input" type="text" placeholder="Логин"/>
+                <input id="email_input"type="email" placeholder="Почта"/>
+                <input id="pass_input" type="password" placeholder="Пароль"/>
+                <input id="repass_input" type="password" placeholder="Повторите пароль"/>
+                <div id="reg_btn">Создать аккаунт</div>
+            </div>
+            <div className="log_block">
+                <p styles="color: #222F2C;">Уже есть аккаунт? &nbsp;</p>
+                <p styles="color: #A4798E; font-weight: 700;">Войти</p>
+            </div>
     </div>
   )
 }
