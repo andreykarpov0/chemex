5fbc84b1b695b8ba8ab6e791490bf6bd5212957d
andreykarpov0 <akarpov863@gmail.com>
2022-03-23 17:49:38 +0300

    added projList

--- ClientApp/src/windows/Home.jsx
+++ ClientApp/src/windows/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [LabeElements,setLabeElements] = useState([])
 
   useEffect(() => {
-    fetch("/gtlabElements")
+    fetch("//projectList")
     .then(res => res.json())
     .then(
         (result) => {
