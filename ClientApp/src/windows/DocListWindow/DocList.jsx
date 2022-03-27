ca182165a6a981fa8d158f7aac8d0a01948322ac
andreykarpov0 <akarpov863@gmail.com>
2022-03-27 20:03:20 +0300

    files routing bug fixed

--- ClientApp/src/windows/DocListWindow/DocList.jsx
+++ ClientApp/src/windows/DocListWindow/DocList.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 import './Home.css';
-import DocView from '...Components/DocumentComponent/DocView';
-import Menu from '...Components/MenuComponent/Menu';
+import DocView from '../Components/DocumentComponent/DocView';
+import Menu from '../Components/MenuComponent/Menu';
 import GetMenuItems from '../Components/GetMenuItems';
 
 export default function Home() {
